a206c6e6cad1cedf0693c3ae236af1ae902b6a1d
Sarah Kinneer <kinneers@gmail.com>
2019-02-16 21:07:05 -0500

    Logic for gathering user data and creating compatibility array complete and tested (can be refactored later)- next: determine most compatible friend and display that friend's information in modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 
-
 // 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
 
 // ```json
-var friends = [{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-}
+var friends = [
+    {
+    "name": "James and the Giant Peach",
+    "photo": "Peaches",
+    "scores": [
+    "1",
+    "2",
+    "5",
+    "2",
+    "3",
+    "5",
+    "2",
+    "1",
+    "2",
+    "3"
+    ]
+    }
 ];
 
 module.exports = friends;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -12,34 +12,53 @@ router.get('/friends', function(req, res) {
 
 //POST route with url `/api/friends` (/api is already set in server.js) used to handle incoming survey results and the compatibility logic
 router.post('/friends', function(req, res) {
-    console.log("Submit Button Working");
-    friends.push(req.body);
-});
-    //var currentUserResults = []; //return the array of results from the current user
+    //Determine the user's most compatible friend:\
 
+    //Bring in the results from the current user's form
+    var currentUserResults = req.body; 
+    
+    //Convert the current user's scores into a simple array of numbers (these will be strings right now)
+    var currentUserScores = currentUserResults.scores;
+        
+    //Initialize an array to hold the differences of each friend in the database to the current user (the index of the lowest number will correspond to the index of the most compatible friend in the database)
+    var compatibility = [];
 
+    //Converts each friends's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`) and compare with current user
+    for (var i = 0; i < friends.length; i++) {
+        var friendScores = friends[i].scores;
+        
+        console.log('Friend in Database: ' + friendScores);
+        console.log('Current user: ' + currentUserScores);
+        var differenceArray = [];
+        for (var j = 0; j < friendScores.length; j++) {
+            //Get the friend score at indicated index
+            var friendNum = parseInt(friendScores[j]);
+            //Get the user score at indicated index
+            var userNum = parseInt(currentUserScores[j]);
+            //Find the absolute value of the difference of the two scores
+            var difference = Math.abs(friendNum - userNum);
+            //Push the difference to differnceArray
+            differenceArray.push(difference);
+        }
+        //Sum the differences in the differenceArray to arrive at a total difference score for this pair of users
+        var totalDifference = differenceArray.reduce((total, amount) => total + amount);
+        //push the total difference for the current friend to that friend's index in the compatibility array
+        compatibility.push(totalDifference);
+    }
+    console.log(compatibility);
 
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example:
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-
-//USE: Math.abs(result);
-
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-
-//USE reduce();
-
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+    
 //    * The closest match will be the user with the least amount of difference.
-
 
 
 // 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 //    * The modal should display both the name and picture of the closest match.
+    //use res.send() to send back the modal with the closest match... I think
+
+
+    //Push the original currentUserResults Object to the friends array
+    friends.push(currentUserResults);
+});
 
 //Exports the router as a Node module
 module.exports = router;
