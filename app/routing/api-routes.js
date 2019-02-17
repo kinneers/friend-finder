89fefac63fd4f7745fef738bda00882ab7de521b
Sarah Kinneer <kinneers@gmail.com>
2019-02-16 23:04:28 -0500

    All logic to determine most compatible friend complete. Next: display modal with bff info on user's page

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -23,38 +23,63 @@ router.post('/friends', function(req, res) {
     //Initialize an array to hold the differences of each friend in the database to the current user (the index of the lowest number will correspond to the index of the most compatible friend in the database)
     var compatibility = [];
 
-    //Converts each friends's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`) and compare with current user
-    for (var i = 0; i < friends.length; i++) {
-        var friendScores = friends[i].scores;
-        
-        console.log('Friend in Database: ' + friendScores);
-        console.log('Current user: ' + currentUserScores);
-        var differenceArray = [];
-        for (var j = 0; j < friendScores.length; j++) {
-            //Get the friend score at indicated index
-            var friendNum = parseInt(friendScores[j]);
-            //Get the user score at indicated index
-            var userNum = parseInt(currentUserScores[j]);
-            //Find the absolute value of the difference of the two scores
-            var difference = Math.abs(friendNum - userNum);
-            //Push the difference to differnceArray
-            differenceArray.push(difference);
+    function getCompatibilityArray(currentUserScores, callback) {
+        //Converts each friends's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`) and compare with current user
+        for (var i = 0; i < friends.length; i++) {
+            var friendScores = friends[i].scores;
+            
+            console.log('Friend in Database: ' + friendScores);
+            console.log('Current user: ' + currentUserScores);
+            var differenceArray = [];
+            for (var j = 0; j < friendScores.length; j++) {
+                //Get the friend score at indicated index
+                var friendNum = parseInt(friendScores[j]);
+                //Get the user score at indicated index
+                var userNum = parseInt(currentUserScores[j]);
+                //Find the absolute value of the difference of the two scores
+                var difference = Math.abs(friendNum - userNum);
+                //Push the difference to differnceArray
+                differenceArray.push(difference);
+            }
+            //Sum the differences in the differenceArray to arrive at a total difference score for this pair of users
+            var totalDifference = differenceArray.reduce((total, amount) => total + amount);
+            //push the total difference for the current friend to that friend's index in the compatibility array
+            console.log(totalDifference);
+            compatibility.push(totalDifference);
         }
-        //Sum the differences in the differenceArray to arrive at a total difference score for this pair of users
-        var totalDifference = differenceArray.reduce((total, amount) => total + amount);
-        //push the total difference for the current friend to that friend's index in the compatibility array
-        compatibility.push(totalDifference);
+        callback();
     }
-    console.log(compatibility);
 
+    var indexOfLowest;
+    //Uses a callback to ensure results from the getCompatibilityArray function are returned prior to calculating the index holding the lowest number (which corresponds to the index of the most compatible friend in the array of friend Objects)
+    getCompatibilityArray(currentUserScores, function() {
+        //Initializes at index 0, then iterates through array and replaces the index number of any numbers found lower than the currently set index
+        indexOfLowest = 0;
+        for (var i = 1; i < compatibility.length; i++) {
+            if (compatibility[i] < compatibility[indexOfLowest]) {
+                indexOfLowest = i;
+            }
+        }
+        return indexOfLowest;
+    });
+
+    console.log(indexOfLowest);
+
+    //Gets the name of most compatible friend
+    var bffName = friends[indexOfLowest].name;
+    console.log(bffName);
+    var bffPhoto = friends[indexOfLowest].photo;
+    console.log(bffPhoto);
     
 //    * The closest match will be the user with the least amount of difference.
 
 
 // 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
 //    * The modal should display both the name and picture of the closest match.
-    //use res.send() to send back the modal with the closest match... I think
+    //use res.send() to send back the modal with the closest match... I think???
+    //var modal = json.stringify($('#myModal').modal('show'));
 
+    res.send(modal);
 
     //Push the original currentUserResults Object to the friends array
     friends.push(currentUserResults);
